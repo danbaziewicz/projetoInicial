f9fbaa7c08a98a4fc31bcbc4a3ba9a60fa051cf0
Danylo Baziewicz <danxbazi@gmail.com>
2025-01-16 10:55:30 -0300

    Adicionando alerts e condicional if.

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,12 @@
+alert('Boas vindas ao nosso site!')
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponível = 1000;
+let mensagemDeErro = "Erro! Preencha todos os campos!";
+alert(mensagemDeErro);
+nome = prompt("Digite seu nome: ");
+idade = prompt("Informe sua idade");
+if(idade <= 18){
+    alert("Pode tirar a habilitação!");
+}
